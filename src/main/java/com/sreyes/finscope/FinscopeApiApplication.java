9ba16fd443ff6f2402b98d1bd008bff6e37b97d6
sebastian-reyes <sebastianreyes2105@gmail.com>
2025-06-27 08:53:20 -0500

    chore: initial commit

---
+++ src/main/java/com/sreyes/finscope/FinscopeApiApplication.java
@@ -0,0 +1,13 @@
+package com.sreyes.finscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinscopeApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinscopeApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sreyes/finscope/FinscopeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreyes.finscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinscopeApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
